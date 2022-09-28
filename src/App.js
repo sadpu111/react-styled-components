3fd3489150967510af227b9301779c52a5c54f14
sadpu111 <dwme7100@gmail.com>
2022-09-28 15:54:37 +0900

    as and attrs

--- src/App.js
+++ src/App.js
@@ -4,21 +4,28 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-background-color: ${(props) => props.bgColor}; // props를 전달받는 함수
-width: 100px;
-height: 100px;
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
 `;
 
-const Circle = styled(Box)` // 확장가능성(Configurable). Box의 props를 그대로 가져오면서 백틱 안의 prop을 추가한다.
-  border-radius: 50px;
+const Input = styled.input.attrs({required: true, minLength:10})`
+  background-color: blue;
 `;
+
+// as="a" 라는 prop을 추가함으로써 Btn의 button태그를 a태그로 변환한다. 즉, styled.a``; 로 변환하는 것.  
 
 function App() {
   return (
     <Father>
-      <Box bgColor = "teal" />
-      <Circle bgColor = "tomato" />
+      <Btn>Log In</Btn>
+      <Btn as="a">Log In</Btn> 
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </Father>
   );
 }
