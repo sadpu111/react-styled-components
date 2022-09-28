1edca8f7ee28023beb2208f40f4c4ae1606843dc
sadpu111 <dwme7100@gmail.com>
2022-09-28 17:38:06 +0900

    theme

--- src/App.js
+++ src/App.js
@@ -1,45 +1,23 @@
 import styled, {keyframes} from "styled-components";
 
-const roatatiomAnimation = keyframes` // keyframes 함수
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  50% {
-    transform: rotate(180deg);
-    border-radius: 100px;
-  }
-  100% {
-    transform: rotate(360deg);
-    border-radius: 0px;
-  }
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor}; // index.js에서 <App>이 <ThemeProvider>안에 있으므로 호출 가능하다.
 `;
 
 const Wrapper = styled.div`
   display: flex;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  animation: ${roatatiomAnimation} 1s linear infinite;
-  display: flex;
+  height: 100vh;
+  width: 100vw;
   justify-content: center;
   align-items: center;
-  span { // styled-components가 아니지만 Box 내부에 있는 태그를 호출 가능하다.
-    font-size: 50px;
-    &:hover { // pseudo selector. span:hover과 동일한 문법.
-      font-size: 100px;}
-  }
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
+
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <span>😍</span>
-      </Box>
+      <Title>Hello!</Title>
     </Wrapper>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
 
+const darkTheme = {
+    textColor: "whitesmoke",
+    backgroundColor: "#111",
+};
+const lightTheme = {
+    textColor: "#111",
+    backgroundColor: "whitesmoke",
+};
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-    <App />
+    <ThemeProvider theme={lightTheme}>
+     <App />
+    </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
